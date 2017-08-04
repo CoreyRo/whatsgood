260c77fe827a77f900c93d99b1a05585703815d2
Corey Rodems <coreyrodems@gmail.com>
2017-08-03 20:48:22 -0700

    made map with customizable pins

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,66 +1,106 @@
-$(document).ready(function()
-{
-	function MainProgram()
-	{	
-		var config = {};		// Initialize Firebase
-		var database;			// Variable to reference the database
-		var lat;
-		var lng;
+$(document).ready(function() {
+			$("#directory").hide()
 
-		config = 
-		{
-			apiKey: "AIzaSyDrsI6iSQqpK66S3C_SDd3UIzGaECV6tqY",
-		    authDomain: "whatsgood-f9823.firebaseapp.com",
-		    databaseURL: "https://whatsgood-f9823.firebaseio.com",
-		    projectId: "whatsgood-f9823",
-		    storageBucket: "whatsgood-f9823.appspot.com",
-		    messagingSenderId: "905439758172"
-		};
+			function MainProgram() {
+				var config = {}; // Initialize Firebase
+				var database; // Variable to reference the database
+				var lat;
+				var lng;
 
-		firebase.initializeApp(config);
+				config = {
+					apiKey: "AIzaSyDrsI6iSQqpK66S3C_SDd3UIzGaECV6tqY",
+					authDomain: "whatsgood-f9823.firebaseapp.com",
+					databaseURL: "https://whatsgood-f9823.firebaseio.com",
+					projectId: "whatsgood-f9823",
+					storageBucket: "whatsgood-f9823.appspot.com",
+					messagingSenderId: "905439758172"
+				};
 
-		database = firebase.database();
+				firebase.initializeApp(config);
 
-		$("#confirmZip").click(function(event)
-		{
-			window.location.href='directory.html';
-			event.preventDefault();
-			var zipcode;
-			var apiKey = "AIzaSyAVeD_VRihMVTcxvIM6mwH6WSEZ-s1kqRo";
-			var queryUrl;
+				database = firebase.database();
 
-			zipcode = $("#zip-input").val();
-			queryUrl = "https://maps.googleapis.com/maps/api/geocode/json?address=" + zipcode + "&key=" + apiKey;
+				$("#confirmZip").click(function(event) {
+					event.preventDefault();
+					$("#start").hide();
+					$("#directory").show();
 
-			$.ajax(
-			{
-				url: queryUrl,
-				method: "GET"
-			}).done(function(response)
-			{
-				console.log(response)
 
-				lat = response.results[0].geometry.location.lat;
-				lng = response.results[0].geometry.location.lng;
+					console.log("Clicked confirmZip")
+					var apiKey = "AIzaSyAuXTlZpy0_PBxrTVDc9p7S_XDpdX0i7po";
+					var zipcode = $("#zip-input").val();
+					console.log("zip " + zipcode);
+					// CreateMap(zipcode, apiKey);
 
-				console.log(lat);
-				console.log(lng);
-			})
+					var queryUrl;
 
-		});
-	}
+					zipcode = $("#zip-input").val();
+					queryUrl = "https://maps.googleapis.com/maps/api/geocode/json?address=" + zipcode + "&key=" + apiKey;
 
-	function CreateMap(location, key)
-	{
-		var main;
+					$.ajax({
+						url: queryUrl,
+						method: "GET"
+					}).done(function(response) {
+						console.log(response)
 
-		main = $("<iframe>");
-		main.attr("width", 600);
-		main.attr("height",450);
-		main.attr("src", "https://www.google.com/maps/embed/v1/search?key=" + key + "&q=bar," + location + " allowfullscreen>");
-		$(".map").append(main);
-	}
+						lat = response.results[0].geometry.location.lat;
+						lng = response.results[0].geometry.location.lng;
 
-	MainProgram();
-	
-});
+						console.log(lat);
+						console.log(lng);
+						initMap(lat, lng);
+
+
+
+					})
+
+
+
+
+				});
+			}
+			$("body").append('<script async defer src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAuXTlZpy0_PBxrTVDc9p7S_XDpdX0i7po&callback=initMap"></script>')
+			var map;
+
+			window.initMap = function(lat, lng) {
+				map = new google.maps.Map(document.getElementById('map'), {
+					zoom: 16,
+					center: new google.maps.LatLng(-33.91722, 151.23064),
+					mapTypeId: 'roadmap'
+
+				});
+
+				var iconBase = 'https://maps.google.com/mapfiles/kml/shapes/';
+				var icons = {
+					parking: {
+						icon: iconBase + 'parking_lot_maps.png'
+					},
+					library: {
+						icon: iconBase + 'library_maps.png'
+					},
+					info: {
+						icon: iconBase + 'info-i_maps.png'
+					}
+				};
+
+				var features = [{
+					position: new google.maps.LatLng(-33.91721, 151.22630),
+					type: 'info'
+				}, {
+					position: new google.maps.LatLng(-33.91727341958453, 151.23348314155578),
+					type: 'library'
+				}];
+
+				// Create markers.
+				features.forEach(function(feature) {
+					var marker = new google.maps.Marker({
+						position: feature.position,
+						icon: icons[feature.type].icon,
+						map: map
+					});
+				});
+
+			}
+
+
+			MainProgram();
